f01487756cb7bd17342c161038ba1396d4fcf282
Ahmeddouiri <133807465+Ahmeddouiri@users.noreply.github.com>
2023-10-01 14:46:41 +0100

    Add files via upload

--- src/component/Task.js
+++ src/component/Task.js
@@ -38,11 +38,8 @@ function Task({ task }) {
 
       <Card.Header style={{ width: '100%' }}> {task.id} :
       <Form.Control readOnly={editing} onClick={() => handleEditing() } style={{ width: '100%', alignItems:'center'}} size="sm"
-<<<<<<< HEAD
+
         type="text" 
-=======
-        type="text" value=""
->>>>>>> 46bf9b4cac937f97406db25085ef56f96c79dc1f
         placeholder={task.title}
         onChange={(e) => setEdittitle(e.target.value)}
         />
